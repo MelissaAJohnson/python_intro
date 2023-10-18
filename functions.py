893305a74ad28ac38d5f68e69b2d97bce7052bd5
Melissa Johnson <9088480+MelissaAJohnson@users.noreply.github.com>
2023-10-18 02:44:30 +0000

    using defaults and params

--- functions.py
+++ functions.py
@@ -1,6 +1,10 @@
-def hello(to):
+def main():
+    name = input("What's your name? ")
+    hello(name)
+
+
+def hello(to="world"):
     print("Hello, ", to)
 
 
-name = input("What's your name? ")
-hello(name)
+main()
