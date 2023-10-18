56f72860e0f983dc6417a049fca413285999041d
Melissa Johnson <9088480+MelissaAJohnson@users.noreply.github.com>
2023-10-18 02:29:23 +0000

    initial commit

---
+++ calculator.py
@@ -0,0 +1,18 @@
+x = int(input("What's x? "))
+y = int(input("What's y? "))
+
+# z = int(x) + int(y)
+
+print(x + y)
+
+# FLOAT
+# https://docs.python.org/3/library/functions.html#round
+
+a = float(input("What's a? "))
+b = float(input("What's b? "))
+
+z = a/b
+
+print(f"{z:,}")
+print(f"{z:.2f}")
+
