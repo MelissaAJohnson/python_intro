da28d6a3b811253eb3ccc3b1656f006e70358c3a
Melissa Johnson <9088480+MelissaAJohnson@users.noreply.github.com>
2023-10-18 02:47:52 +0000

    calculate using function

--- calculator.py
+++ calculator.py
@@ -1,3 +1,4 @@
+"""
 x = int(input("What's x? "))
 y = int(input("What's y? "))
 
@@ -18,4 +19,13 @@ print(f"{z:,}")
 
 # specify number of characters to present
 print(f"{z:.2f}")
+"""
 
+def main():
+    x = int(input("What's x? "))
+    print("x squared is", square(x))
+
+def square(n):
+    return n * n
+
+main()
